77bacf98c623aad220001d87aac56049fcd20abb
socketflow <46785285+socketflow@users.noreply.github.com>
2020-08-09 06:22:10 +0000

    revert previous change

--- v2log-cleaner-Debian.sh
+++ v2log-cleaner-Debian.sh
@@ -7,8 +7,8 @@ mv /var/log/v2ray/error.log /var/log/v2ray/error.log.bak
 mv /var/log/v2ray/access.log /var/log/v2ray/access.log.bak
 touch /var/log/v2ray/error.log
 touch /var/log/v2ray/access.log
-chmod 664 /var/log/v2ray/error.log
-chmod 664 /var/log/v2ray/access.log
+chmod 666 /var/log/v2ray/error.log
+chmod 666 /var/log/v2ray/access.log
 echo "Log Files Cleaned"
 
 systemctl start v2ray
