bf47fce78b51250203811878f698f0f6ae0d373d
socketflow <46785285+socketflow@users.noreply.github.com>
2020-08-10 07:15:14 +0000

    Create acme-to-v2ray.sh

---
+++ acme-to-v2ray.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+acme.sh --installcert -d [domain.com] --ecc  --fullchain-file /usr/local/etc/v2ray/v2ray.crt  --key-file /usr/local/etc/v2ray/v2ray.key
+echo "New Certificates Installed to v2ray"
+
+systemctl restart v2ray
+echo "v2ray Restarted"
