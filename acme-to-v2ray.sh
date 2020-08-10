953565a4f37542b9cebe9a1c7854c8a29334c360
socketflow <46785285+socketflow@users.noreply.github.com>
2020-08-10 07:30:16 +0000

    Ensure Read Persmission is Granted

--- acme-to-v2ray.sh
+++ acme-to-v2ray.sh
@@ -3,5 +3,9 @@
 /root/.acme.sh/acme.sh --installcert -d [domain.com] --ecc  --fullchain-file /usr/local/etc/v2ray/v2ray.crt  --key-file /usr/local/etc/v2ray/v2ray.key
 echo "New Certificates Installed to v2ray"
 
+chmod 644 /usr/local/etc/v2ray/v2ray.crt
+chmod 644 /usr/local/etc/v2ray/v2ray.key
+echo "Read Permission Granted"
+
 systemctl restart v2ray
 echo "v2ray Restarted"
