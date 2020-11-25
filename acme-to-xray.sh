2bf38f53924d0239b796e7a35e24681074ada404
socketflow <46785285+socketflow@users.noreply.github.com>
2020-11-25 12:36:45 +0000

    Create acme-to-xray.sh

---
+++ acme-to-xray.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+/root/.acme.sh/acme.sh --installcert -d [domain.com] --ecc  --fullchain-file /usr/local/etc/xray/xray.crt  --key-file /usr/local/etc/xray/xray.key
+echo "New Certificates Installed to xray"
+
+chmod 644 /usr/local/etc/xray/xray.crt
+chmod 644 /usr/local/etc/xray/xray.key
+echo "Read Permission Granted"
+
+systemctl restart xray
+echo "xray Restarted"
+
