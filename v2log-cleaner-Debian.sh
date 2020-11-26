9d4f65c1df68435b72589664a3afb519ee3765c0
socketflow <46785285+socketflow@users.noreply.github.com>
2020-11-26 12:55:26 +0000

    disable service auto restart

--- v2log-cleaner-Debian.sh
+++ v2log-cleaner-Debian.sh
@@ -11,5 +11,6 @@ chmod 666 /var/log/v2ray/error.log
 chmod 666 /var/log/v2ray/access.log
 echo "Log Files Cleaned"
 
-systemctl start v2ray
-echo "v2ray Restarted"
+# this is moved from this script to crontab, so as to keep disabled service froming restarting
+# systemctl start v2ray
+# echo "v2ray Restarted"
