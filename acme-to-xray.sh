8f2f372274dcd62f1e24ccffd5f7904d614367b0
socketflow <46785285+socketflow@users.noreply.github.com>
2020-11-26 12:50:18 +0000

    disable service auto restart

--- acme-to-xray.sh
+++ acme-to-xray.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+systemctl stop xray
+echo "xray stopped"
+
 /root/.acme.sh/acme.sh --installcert -d [domain.com] --ecc  --fullchain-file /usr/local/etc/xray/xray.crt  --key-file /usr/local/etc/xray/xray.key
 echo "New Certificates Installed to xray"
 
@@ -7,5 +10,6 @@ chmod 644 /usr/local/etc/xray/xray.crt
 chmod 644 /usr/local/etc/xray/xray.key
 echo "Read Permission Granted"
 
-systemctl restart xray
-echo "xray Restarted"
+# this is left to crontab, to avoid diabled service gets restarted
+# systemctl start xray
+# echo "xray started"
