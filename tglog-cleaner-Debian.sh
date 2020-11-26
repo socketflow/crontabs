efff900cd791cbcf83589a4355d272d740875cc6
socketflow <46785285+socketflow@users.noreply.github.com>
2020-11-26 12:54:47 +0000

    disable service auto restart

--- tglog-cleaner-Debian.sh
+++ tglog-cleaner-Debian.sh
@@ -8,5 +8,6 @@ touch /var/log/trojan-go/tglog.log
 chmod 666 /var/log/trojan-go/tglog.log
 echo "Trojan-Go Log Files Cleaned"
 
-systemctl start trojan-go
-echo "Trojan-Go Restarted"
+# this is moved from this script to crontab, so as to keep disabled service froming restarting
+# systemctl start trojan-go
+# echo "Trojan-Go Restarted"
