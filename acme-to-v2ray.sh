8365d3a8686f53440f79614a61b1faddb94fd749
socketflow <46785285+socketflow@users.noreply.github.com>
2020-11-26 12:52:26 +0000

    disable service auto restart

--- acme-to-v2ray.sh
+++ acme-to-v2ray.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+systemctl stop v2ray
+echo "v2ray stopped"
+
 /root/.acme.sh/acme.sh --installcert -d [domain.com] --ecc  --fullchain-file /usr/local/etc/v2ray/v2ray.crt  --key-file /usr/local/etc/v2ray/v2ray.key
 echo "New Certificates Installed to v2ray"
 
@@ -7,5 +10,6 @@ chmod 644 /usr/local/etc/v2ray/v2ray.crt
 chmod 644 /usr/local/etc/v2ray/v2ray.key
 echo "Read Permission Granted"
 
-systemctl restart v2ray
-echo "v2ray Restarted"
+# this is moved from this script to crontab, so as to keep disabled service froming restarting
+# systemctl restart v2ray
+# echo "v2ray Restarted"
