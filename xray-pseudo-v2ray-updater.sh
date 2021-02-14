5f5b5a2a66d8007615be56ababfac684f0d37bdd
socketflow <46785285+socketflow@users.noreply.github.com>
2021-02-14 15:16:30 +0000

    Update xray-pseudo-v2ray-updater.sh

--- xray-pseudo-v2ray-updater.sh
+++ xray-pseudo-v2ray-updater.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-cd xray-dl
-echo 'Current Directory is: '$PWD''
+INSTALL_VERSION="v1.3.0"
+
+cd /root/xray-dl
+echo ''
+echo '1. current folder is '$PWD''
 echo ''
 
-wget https://github.com/XTLS/Xray-core/releases/download/v1.1.5/Xray-linux-64.zip
-echo 'New Xray Downloaded'
+wget https://github.com/XTLS/Xray-core/releases/download/$INSTALL_VERSION/Xray-linux-64.zip
+echo '2. New Xray Downloaded'
 echo ''
 
 unzip Xray-linux-64.zip
-echo 'New Xray Extracted'
+echo ''
+echo '3. New Xray Extracted'
 echo ''
 
 chmod +x xray
-echo 'New xray execution previlege provided'
+echo '4. New xray execution previlege provided'
 echo ''
 
 mv xray v2ray
-echo 'New Pseudo v2ray created'
+echo '5. New Pseudo v2ray created'
 echo ''
 
 cp /usr/local/bin/v2ray /usr/local/bin/v2ray.bak
-echo 'old pseudo-v2ray backed up'
+echo '6. old pseudo-v2ray backed up'
 echo ''
 
-mv v2ray /usr/local/bin/
-echo 'new pseudo v2ray installed'
+mv v2ray /usr/local/bin/ && cd /root
+echo '7. new pseudo v2ray installed'
 echo ''
 
 rm /root/xray-dl/*
-echo 'xray-dl folder cleared'
+echo '8. xray-dl folder cleared'
 echo ''
 
 systemctl restart v2ray
-echo 'pseudo v2ray restarted'
+echo '9. pseudo-v2ray restarted'
 echo ''
