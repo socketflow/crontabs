c0cbe11c07a60006be191e84af31de834b60ace1
socketflow <46785285+socketflow@users.noreply.github.com>
2021-02-14 15:34:04 +0000

    Update xray-pseudo-v2ray-updater.sh

--- xray-pseudo-v2ray-updater.sh
+++ xray-pseudo-v2ray-updater.sh
@@ -2,7 +2,7 @@
 
 INSTALL_VERSION="v1.3.0"
 
-cd /root/xray-dl
+mkdir /root/xray-dl && cd /root/xray-dl
 echo ''
 echo '1. current folder is '$PWD''
 echo ''
@@ -32,7 +32,7 @@ mv v2ray /usr/local/bin/ && cd /root
 echo '7. new pseudo v2ray installed'
 echo ''
 
-rm /root/xray-dl/*
+rm /root/xray-dl/* && rm -d /root/xray-dl
 echo '8. xray-dl folder cleared'
 echo ''
 
